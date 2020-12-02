25763a19d5cb043e52d201c7ffe373dae1e55c74
Teresia Ngigi <tshiks22@outlook.com>
2020-12-02 12:13:17 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonQuote();
+
+            for(int x = 1; x >=5; ++x)
+            {
+                //Console.WriteLine($"{QuoteGenerator.KanyeQuote} ");
+                //
+                //and {QuoteGenerator.RonQuote()}");
+            }
+        }
+    }
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KanyeAndRon
+{
+    public class QuoteGenerator
+    {
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine("Kanye:");
+            Console.WriteLine(kanyeQuote);
+            Console.WriteLine("------");
+
+        }
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray
+                .Parse(ronResponse)
+                .ToString()
+                .Replace('[', ' ')
+                .Replace(']', ' ').Trim();
+
+            Console.WriteLine("Ron:") ;
+            Console.WriteLine(ronQuote);
+            Console.WriteLine("------");
+        }
+    }
+}
